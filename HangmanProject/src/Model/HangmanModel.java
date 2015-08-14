be13d0ea10c1748e3028214d2bfa77abd17e5558
Evan Davies <evanjohndavies@yahoo.com>
2015-08-14 13:26:07 -0500

    Test Branching

--- HangmanProject/src/Model/HangmanModel.java
+++ HangmanProject/src/Model/HangmanModel.java
@@ -6,7 +6,8 @@ public class HangmanModel {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("again");
-		
+		System.out.println("and again");
+			
 
 	}
 
